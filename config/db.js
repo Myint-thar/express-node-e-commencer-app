0649fb81b88e9a6ce99c2f8852df5a985e75720d
Your Name <you@example.com>
2026-08-29 03:01:17 +0630

    e-c-app completed

--- config/db.js
+++ config/db.js
@@ -4,9 +4,10 @@ require('dotenv').config();
 const sequelize = new Sequelize(
   process.env.DB_NAME || 'ecommerce_db',
   process.env.DB_USER || 'root',
-  process.env.DB_PASSWORD || '',
+  process.env.DB_PASS || '',
   {
     host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 3306, 
     dialect: 'mysql',
     logging: false 
   }
